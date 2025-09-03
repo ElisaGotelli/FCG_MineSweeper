175bb7bb91eeea73228e2d871c367a0ac7ec1aca
ElisaGotelli <s5666586@studenti.unige.it>
2025-09-03 16:55:51 +0200

    metto il rettangolo in 01

--- 01_starting_background.cpp
+++ 01_starting_background.cpp
@@ -2,23 +2,54 @@
 
 //------------FINESTRA------------------
 const char* window_title = "01 - start_background";
-const unsigned window_width = 1200;
-const unsigned window_height = 900;
+const unsigned window_width = 800;
+const unsigned window_height = 600;
 const float max_frame_rate = 60;
 
+//start size 
+const sf::Vector2f start_size = {600.0, 400.0};
 
+struct Start
+{
+    sf::Vector2f size;
+    sf::Vector2f pos;
+    sf::Texture texture;
 
-//-------------griglia---------------
-const float wall_horizontal_displacement = 50;
-const float wall_vertical_displacement = 100;
-const float wall_horizontal_size = 750;
-const float wall_vertical_size = 150;
-const unsigned int blocks_horizontal_num = 16;
-const unsigned int blocks_vertical_num = 6;
-sf::Color block_fill_color = sf::Color(220, 200, 20);
-float block_outline_thickness = 2.0;
-sf::Color block_outline_color = sf::Color(220, 140, 90);
+    Start ();
+    void draw (sf::RenderWindow& window);
+};
 
+struct State
+{
+    Start start; 
+
+    State () {}
+    void draw (sf::RenderWindow& window);
+};
+
+Start::Start ()
+{
+    size = start_size; 
+    float start_px = (window_width  / 2.f) - (start_size.x / 2.f);
+    float start_py = (window_height / 2.f) - (start_size.y / 2.f);
+    pos = {start_px, start_py};
+}
+
+//--------DRAW---------------
+void Start::draw (sf::RenderWindow& window)
+{
+    sf::RectangleShape s (size);
+    s.setPosition (pos);
+    s.setFillColor(sf::Color(210, 180, 140));
+    s.setOutlineThickness(20.f);
+    s.setOutlineColor(sf::Color(92, 51, 23));
+    window.draw (s);
+}
+
+void State::draw (sf::RenderWindow& window)
+{
+    start.draw (window);
+}
 
 //--------------EVENTI-----------------
 
@@ -26,6 +57,7 @@ void handle_close (sf::RenderWindow& window)
 {
     window.close();
 }
+
 void handle_resize (const sf::Event::Resized& resized, sf::RenderWindow& window)
 {
     float aspect = static_cast<float>(window_width)/static_cast<float>(window_height);
@@ -35,6 +67,7 @@ void handle_resize (const sf::Event::Resized& resized, sf::RenderWindow& window)
         ws = {ws.x,static_cast<unsigned>(ws.x/aspect)};
         else
         ws = {static_cast<unsigned>(ws.y*aspect),ws.y};
+
     window.setSize(ws);
 }
 
@@ -44,7 +77,9 @@ int main()
 {
     sf::RenderWindow window (sf::VideoMode ({window_width, window_height}), window_title);
     window.setFramerateLimit (max_frame_rate);
-    window.setMinimumSize(window.getSize());
+    window.setMinimumSize(window.getSize()); 
+
+    State state;
 
     while (window.isOpen())
     {
@@ -55,7 +90,9 @@ int main()
         );
 
         // display
-        window.clear (sf::Color::Black);
-        window.display ();
+        window.clear(sf::Color(144, 238, 144)); // verde chiaro di sfondo
+        state.draw(window);
+        window.display();
+
     }
 }
